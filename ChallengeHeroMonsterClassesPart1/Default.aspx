058579dd7dbc6489d1e06516dc42c703721e664d
Travis Brooks <tbrooks3141@gmail.com>
2017-11-28 15:22:40 -0800

    C# / ASP.NET drill

---
+++ ChallengeHeroMonsterClassesPart1/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeHeroMonsterClassesPart1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 130px;
+            height: 150px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="display: inline-block; text-align: center;">
+        <img alt="" class="auto-style1" src="images/eleven.jpg" /><br />
+        <asp:Label ID="elevenLabel" runat="server"></asp:Label>
+    </div>
+    <div style="margin-left: 100px; display: inline-block; text-align: center;">
+        <img alt="" class="auto-style1" src="images/dg.jpg" /><br />
+        
+        <asp:Label ID="demogorgonLabel" runat="server"></asp:Label>
+        
+    </div>
+
+    <div style="margin-top: 50px;">
+        <asp:Label ID="actionLabel" runat="server"></asp:Label>
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ ChallengeHeroMonsterClassesPart1/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+            hero.Name = "Eleven";
+            hero.Health = 100;
+            hero.DamageMax = 15;
+            hero.AttackBonus = 10;
+            hero.LabelName = "elevenLabel";
+
+            Character monster = new Character();
+            monster.Name = "Demogorgon";
+            monster.Health = 666;
+            monster.DamageMax = 35;
+            monster.AttackBonus = 0;
+            monster.LabelName = "demogorgonLabel";
+            string action = "";
+            monster.Defend(hero.Attack(), out action);
+            writeAction(action);
+            hero.Defend(monster.Attack(), out action);
+            writeAction(action);
+            updateStats(hero);
+            updateStats(monster);
+        }
+
+        private void updateStats(Character character)
+        {
+            Label label = FindControl(character.LabelName) as Label;
+            label.Text = character.Name + "<br />HP: " + character.Health; 
+        }
+
+        private void writeAction(string action)
+        {
+            actionLabel.Text += action + "<br />";
+        }
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMax { get; set; }
+        public int AttackBonus { get; set; }
+        public string LabelName { get; set; }
+
+        public int Attack()
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+            return random.Next(this.DamageMax + 1) + this.AttackBonus;
+        }
+
+        public void Defend(int damage, out string action)
+        {
+            this.Health -= damage;
+            action = string.Format("{0} was hit for {1} damage.", this.Name, damage);
+        }
+
+    }
+
+}
